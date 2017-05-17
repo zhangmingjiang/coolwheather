f67e955dbf810c88080d10f735654ede4224f2aa
zhangmj <zhang.mj@bblbs.net>
2017-05-17 09:25:50 +0800

    modal,util

---
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -0,0 +1,46 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by zhangmj on 2017/5/17.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
